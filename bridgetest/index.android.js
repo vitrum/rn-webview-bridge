77a02017d2421cc5d25b3902bc522a6d8a24d216
vitrum <xingang.zhu@sy-fintech.com>
2018-10-09 14:27:25 +0800

    Add android

--- bridgetest/index.android.js
+++ bridgetest/index.android.js
@@ -2,6 +2,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import Entry from "./Entry";
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Entry);
