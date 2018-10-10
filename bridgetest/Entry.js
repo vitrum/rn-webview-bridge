f2e2d8bb90446fa545c08082d3cb40d0cca269ad
caiyuke <28084643@qq.com>
2018-10-10 15:32:51 +0800

    lost

--- bridgetest/Entry.js
+++ bridgetest/Entry.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, View, Text, WebView, ToastAndroid, Linking, StatusBar, TouchableHighlight, Alert} from 'react-native';
-import { Button ,TextareaItem , Accordion} from 'antd-mobile-rn';
+import { List,InputItem ,Modal,TextareaItem , Accordion,Toast,ImagePicker as AntImagePicker} from 'antd-mobile-rn';
+import ImagePicker from 'react-native-image-picker';
 import Mock from "mockjs";
-
+//https://www.jianshu.com/p/727c9d4c080c  关于ImagePicker的API及使用说明
 
 export default class Entry extends Component <Props> {
   constructor(props){
@@ -14,35 +15,67 @@ export default class Entry extends Component <Props> {
         times:0
       };
   }
+
   //实际处理客户端请求的函数
   handle(sendMesg){
-    const { action  , payload } = sendMesg;
+    const { action  , payload = {} ,sendId} = sendMesg;
     let result = '';
     switch(action){
-      case 'UPPERCASE' : result = String(payload.str).toLocaleUpperCase() ; break;
-      case 'LOGIN' : result = {
-        userId:Mock.mock({ 'regexp': /\d{3,5}/ }).regexp ,
-        nickName:Mock.Random.csentence(8),
-        loginTime:+ new Date(),
-        token:Mock.mock('@word(12, 15)')
-      } ;break;
-      default : result = '当前ACTION尚未定义和实现'; break;
+      case 'PIC_AND_VEDIO' :
+      ImagePicker.showImagePicker({
+        ...payload
+      }, async response => {
+          let status = false;
+          if (response.didCancel) {
+              result = 'User cancelled image picker';
+              console.log(result);
+          } else if (response.error) {
+              result = response.error;
+              console.log('ImagePicker Error: ', response.error);
+          } else if (response.customButton) {
+              result = response.customButton;
+              console.log('User tapped custom button: ', response.customButton)
+          } else {
+            status = true;
+            result = response.data;
+          }
+          this.sendNativeResult({
+            result,
+            status ,
+            sendId,
+            action
+          })
+        })
+      break;
+
+      case 'LOGIN' : 
+        //login类型的action 处理 : 显示native的登录框，在操作结果中获取对应信息并回发 webview
+        this.setState({loginModalState:true,splitImage:sendId}) ; 
+      break;
+
+      default : 
+        result = '当前ACTION尚未定义和实现';
+        this.sendNativeResult({
+          result,
+          status :String(Math.random()).slice(-1)%2,
+          sendId,
+          action
+        })
+       break;
     }
-    return {
-      status:String(Math.random()).slice(-1)%2,
-      result
-    };
   }
+  //接收来自 webview发送的信息
   onWebViewMessage(event){
     let {times} = this.state;
     this.setState({
       reciveHTML : event.nativeEvent.data ,
-      times:times+=1
+      times:times+=1,
     });
       
     //解构收到的信息
     const  {action , payload, sendId } = JSON.parse(event.nativeEvent.data) || {};
-    //先回执一个收到的通知。
+
+    //先发送一个通知给webview 告知已收到。
     this.myWebView.postMessage(
       JSON.stringify({
         action,sendId,
@@ -51,48 +84,140 @@ export default class Entry extends Component <Props> {
       })
     );
     
-    const handleData = this.handle({payload,action});
+    //处理具体的task
+    this.handle({payload,action,sendId});
+  }
+
+  //把native处理的具体结果 发送给webview
+  sendNativeResult(handleData){
+    let updateState = handleData.updateState || {};
+    if(handleData.updateState){ delete handleData.updateState;}
+
     const returnData = {
-      action,
-      sendId,
       type:'handleResult',
-      payload:{
-        ...payload ,
-        result : handleData.result
-      },
-      status:handleData.status
+      ...handleData
+      // action,
+      // sendId,
+      // payload:{
+      //   ...payload ,
+      //   result : handleData.result
+      // },
+      // status:handleData.status
     };
 
     this.myWebView.postMessage(
       JSON.stringify(returnData)
     );
-    
+  
     this.setState({
-      returnHTML : JSON.stringify(returnData)
+      returnHTML : JSON.stringify(returnData),
+      ...updateState
     });
+  }
+
+  //LOGIN提交函数
+  loginResult(status){
+    const {splitImage,nickName,pwd} = this.state;
+    if(status && (!nickName || !pwd)){
+      this.setState({
+        tip:true,
+        tipMesg:'请输入用户名和密码'
+      });
+      return ;
+    }
+    let result = {
+      pwd,
+      nickName,
+      loginTime:+ new Date(),
+      token:Mock.mock('@word(12, 15)'),
+      userId:Mock.mock({ 'regexp': /\d{3,5}/ }).regexp 
+    } ;
+    if(!status){result = {mesg:'User cancel login.'}}
+    this.sendNativeResult({
+      result,
+      status ,
+      sendId:splitImage,
+      action:'LOGIN',
+      updateState:{
+        loginModalState:false,
+        pwd:'',
+        nickName:'',
+        tip:false
+      }
+    })
   }
   render() {
     const {reciveHTML,returnHTML ,times} = this.state;
+    const {loginModalState ,nickName , pwd ,tip , tipMesg} = this.state;
     return (
       <View style={styles.container}>
-          <Text>This is the RN area。 接收{times}次消息。</Text>
 
-         <Accordion>
-          <Accordion.Panel header="HTML发送的信息"  >
-            {
-              reciveHTML?
-              <TextareaItem rows={4}  autoHeight style={{ paddingVertical: 5 }} defaultValue={reciveHTML} editable={false} />
-              :<Text>暂无信息</Text>
+        <Text>This is the RN area。 接收{times}次消息。</Text>
+
+        <Accordion>
+        <Accordion.Panel header="HTML发送的信息"  >
+          {
+            reciveHTML?
+            <TextareaItem rows={4}  autoHeight style={{ paddingVertical: 5 }} defaultValue={reciveHTML} editable={false} />
+            :<Text>暂无信息</Text>
+          }
+        </Accordion.Panel>
+        <Accordion.Panel header="处理action后回执(回发HTML)" >
+          {
+            returnHTML?
+            <TextareaItem rows={4}  autoHeight style={{ paddingVertical: 5 }}  defaultValue={returnHTML} editable={false} />
+            :<Text>暂无信息</Text>
+          }
+        </Accordion.Panel>
+        </Accordion>
+          
+        <Modal
+          title="Login"
+          transparent={true}
+          maskClosablet={false}
+          visible={loginModalState}
+          closablet={true}
+          footer={[
+            { text: '登录失败', onPress: () => this.loginResult(false) },
+            { text: '登录成功', onPress: () => this.loginResult(true) },
+          ]}
+        >
+          <View style={{ paddingVertical: 20 }}>
+            <List>
+              <InputItem clear={true} placeholder={"请输入用户名"} value={nickName} onChange={val=>this.setState({nickName:val})}>用户名：</InputItem>
+              <InputItem type="password" clear={true} placeholder={"请输入密码"} value={pwd}  onChange={val=>this.setState({pwd:val})}>密  码：</InputItem>
+            </List>
+            { tip && <Text style={styles.tipMesg}>{tipMesg}</Text> }
+          </View>
+        </Modal>
+
+        {/* <AntImagePicker
+          onChange={(file,operationType,index)=>{
+            this.setState({
+              returnHTML : JSON.stringify({
+                file,operationType,index
+              }),
+            });
+          }}
+          onImageClick={
+            (index,file)=>{
+              this.setState({
+                returnHTML : JSON.stringify({
+                  file,index
+                }),
+              });
             }
-          </Accordion.Panel>
-          <Accordion.Panel header="处理action后回执(回发HTML)" >
-            {
-              returnHTML?
-              <TextareaItem rows={4}  autoHeight style={{ paddingVertical: 5 }}  defaultValue={returnHTML} editable={false} />
-              :<Text>暂无信息</Text>
-            }
-          </Accordion.Panel>
-          </Accordion>
+          }
+          multiple={true}
+          onAddImageClick={(a)=>{window.alert(a)}}
+          files={[{
+            url: 'https://zos.alipayobjects.com/rmsportal/PZUUCKTRIHWiZSY.jpeg',
+            id: '2121',
+          }, {
+            url: 'https://zos.alipayobjects.com/rmsportal/hqQWgTXdrlmVVYi.jpeg',
+            id: '2122',
+          }]}
+        /> */}
 
         
         <WebView 
@@ -114,7 +239,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor:'#009999',
     padding:10,
-    paddingTop:30,
+    paddingTop:30
+  },
+  tipMesg:{
+    color:'red',
+    paddingLeft:18,
+    paddingTop:5
   },
   webview: {
     height: 250,
--- bridgetest/www/Brige.js
+++ bridgetest/www/Brige.js
@@ -1,4 +1,5 @@
 (function(object){
+    //参数类型检测
     var checkPrams = {
         isString:function(parm){
             return Object.prototype.toString.call(parm) === "[object String]";
@@ -10,12 +11,18 @@
             return Object.prototype.toString.call(parm) === "[object Function]";
         }
     };
+    //桥接对象
     var _WebviewBrige = {
+        //为 webview发送到native的每个消息 创建唯一ID
         _createSendId:function() {
             return Math.floor((1 + Math.random()) * 0x20000).toString(16);
         },
+        //保存所有从webview发送出去的消息队列信息
         _sendList:{}, 
+        //真正发送消息给native的函数
         send:function(action,data ,succCallback , errCallback){
+            //参数信息检测
+
             if(arguments.length < 4){
                 console.warn('This function must input 4 parameters : action(String) , data(Object) , succCallback(Function) , errCallback(Function).');
                 return false;
@@ -37,31 +44,38 @@
                 return false;
             }
             var _thisSendID = this._createSendId();
-
+            //组建消息包裹
             var sendMesg  = {
                 action : action.toLocaleUpperCase(),
                 payload   : data,
                 sendId : _thisSendID
             };
+            //从webview发送消息到native
             window.postMessage(JSON.stringify(sendMesg));
+            //保存消息结果的处理函数
             sendMesg.resolve  =  succCallback;
             sendMesg.reject = errCallback;
+            //保存当前消息到队列中
             this._sendList[_thisSendID] = sendMesg;
         }
     };
-    var debug  = debug || console;
+    var debug  = window.debug || console;
 
+    //webview监听从native发送来的消息
     window.document.addEventListener('message',function(oEvent){
         var oReturnData = JSON.parse(oEvent.data);
+        //通知类型的消息
         if(oReturnData.type === 'notice'){
             debug.log(oReturnData.action+'已发送成功并被接收。');
             return ;
         }
+
          var sendInfo = _WebviewBrige._sendList[oReturnData.sendId] ;
         if(!sendInfo){
             debug.error(oReturnData.action+'包括信息丢失') ;
         }else{
             debug.log(oReturnData.action+'已处理完毕并收到回执');
+            //根据native处理的状态，把结果传递给消息对应的处理函数中
             if(oReturnData.status){
                 sendInfo.resolve(oReturnData);
             }else{
