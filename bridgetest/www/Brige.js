774bd1064c68b75a74bf33e3cc3e997859d1e80b
Simon <y294503398@163.com>
2018-10-11 18:41:29 +0800

    add support  promise type callBack

--- bridgetest/www/Brige.js
+++ bridgetest/www/Brige.js
@@ -11,6 +11,7 @@
             return Object.prototype.toString.call(parm) === "[object Function]";
         }
     };
+    var debug  = window.debug || console;
     //桥接对象
     var _WebviewBrige = {
         //为 webview发送到native的每个消息 创建唯一ID
@@ -23,8 +24,8 @@
         send:function(action,data ,succCallback , errCallback){
             //参数信息检测
 
-            if(arguments.length < 4){
-                console.warn('This function must input 4 parameters : action(String) , data(Object) , succCallback(Function) , errCallback(Function).');
+            if(arguments.length < 2){
+                console.warn('This function must input at  least 2  parameters : action(String) , data(Object) . others  parameters : succCallback(Function)(Optional) , errCallback(Function)(Optional).');
                 return false;
             }
             if(!checkPrams.isString(action)){
@@ -35,11 +36,11 @@
                 console.warn('The [data] parameter of the [send] function must be [Object]');
                 return false;
             }
-            if(!checkPrams.isFunction(succCallback)){
+            if( succCallback && !checkPrams.isFunction(succCallback)){
                 console.warn('The [succCallback] parameter of the [send] function must be [Function]');
                 return false;
             }
-            if(!checkPrams.isFunction(errCallback)){
+            if(errCallback && !checkPrams.isFunction(errCallback)){
                 console.warn('The [errCallback] parameter of the [send] function must be [Function]');
                 return false;
             }
@@ -47,21 +48,28 @@
             //组建消息包裹
             var sendMesg  = {
                 action : action.toLocaleUpperCase(),
-                payload   : data,
+                payload: data,
                 sendId : _thisSendID
             };
             //从webview发送消息到native
             window.postMessage(JSON.stringify(sendMesg));
-            //保存消息结果的处理函数
-            sendMesg.resolve  =  succCallback;
-            sendMesg.reject = errCallback;
+
+            //保存消息结果回调类型处理函数(如果有的话)
+            if(succCallback){ sendMesg.resolve  =  succCallback;}
+            if(errCallback){  sendMesg.reject = errCallback;}
+
             //保存当前消息到队列中
             this._sendList[_thisSendID] = sendMesg;
+
+            var _sendList = this._sendList;
+            return new Promise(function(resolve ,reject){
+                _sendList[_thisSendID].promiseResolve = resolve;
+                _sendList[_thisSendID].promiseReject = reject;
+            });
         }
     };
-    var debug  = window.debug || console;
+    
 
-    //webview监听从native发送来的消息
     window.document.addEventListener('message',function(oEvent){
         var oReturnData = JSON.parse(oEvent.data);
         //通知类型的消息
@@ -69,20 +77,21 @@
             debug.log(oReturnData.action+'已发送成功并被接收。');
             return ;
         }
-
          var sendInfo = _WebviewBrige._sendList[oReturnData.sendId] ;
         if(!sendInfo){
-            debug.error(oReturnData.action+'包括信息丢失') ;
+            debug.error(oReturnData.action+'包裹信息丢失') ;
         }else{
             debug.log(oReturnData.action+'已处理完毕并收到回执');
             //根据native处理的状态，把结果传递给消息对应的处理函数中
             if(oReturnData.status){
-                sendInfo.resolve(oReturnData);
+                sendInfo.resolve && sendInfo.resolve(oReturnData);
+                sendInfo.promiseResolve && sendInfo.promiseResolve(oReturnData);
             }else{
-                sendInfo.reject(oReturnData);
+                sendInfo.reject && sendInfo.reject(oReturnData);
+                sendInfo.promiseReject && sendInfo.promiseReject(oReturnData);
             }
             delete _WebviewBrige._sendList[oEvent.sendId];
         }
-    })
+    });
     object.WebviewBrige = _WebviewBrige ;
 })(window);
